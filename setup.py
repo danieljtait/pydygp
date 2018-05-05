92b79c65d75bba2504423d37e049dd4195bce11a
danieljtait <tait.djk@gmail.com>
2018-05-05 16:13:52 +0100

    created docs

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(name='pydygp',
-      version='0.1',
+      version='0.1.01',
       author='Daniel Tait',
+      email='tait.djk@gmail.com',
+      url='http://github.com/danieljtait/pydygp',
       license='MIT',
       packages=['pydygp'],
       zip_safe=False)      
